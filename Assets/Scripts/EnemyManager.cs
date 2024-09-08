88dd83e663925a74481d01496711e738f22e27ed
KennethMcLachlan <kennethmclachlan11@gmail.com>
2024-09-08 15:20:04 -0700

    NavMesh and navAgent implemented

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyManager : MonoBehaviour
 {
@@ -13,6 +14,6 @@ public class EnemyManager : MonoBehaviour
 
     void Update()
     {
-        
+        GetComponent<NavMeshAgent>().destination = player.transform.position;
     }
 }
